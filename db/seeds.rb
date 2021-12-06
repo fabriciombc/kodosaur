2a8e8954c9107cfdf72dbf34abaea7e1dfe4f487
Matheus Andrade <matheuslincon50@gmail.com>
2021-12-06 14:35:03 -0500

    seeds completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,11 @@ User.create!(nickname: 'sebi_2014', email: 'sebi@test.com', password: '123123')
 User.create!(nickname: 'manuzinha_2014', email: 'manu@test.com', password: '123123')
 User.create!(nickname: 'duda_2014', email: 'duda@test.com', password: '123123')
 
-
 puts "Creating challenges..."
-Challenge.create(objective: "Reach the goal", level: {
+# -----------------------------------------
+# Level 1
+# Obj: Ajude Kodo a sair de casa
+Challenge.create(objective: "Help Kodo leave the house", level: {
   map:[
     [0, 1, 0, 0, 1],
     [0, 0, 0, 0, 0],
@@ -37,8 +39,10 @@ Challenge.create(objective: "Reach the goal", level: {
   theme:'default'
 }
 )
-
-Challenge.create(objective: "Reach the goal", level: {
+# -----------------------------------------
+# Level 2
+# Obj : Ajude Kodo a chegar no mercado.
+Challenge.create(objective: "Help Kodo arrive at supermarket", level: {
   map:[
     [0, 1, 0, 0, 0],
     [0, 1, 0, 1, 0],
@@ -48,6 +52,138 @@ Challenge.create(objective: "Reach the goal", level: {
   ],
   player: {
     x:0,
+    y:0
+  },
+  goal:{
+    x:4,
+    y:4
+  },
+  theme:'default'
+}
+)
+# -----------------------------------------
+# Level 3
+# Obj : Ajude Kodo a pegar o Leite
+Challenge.create(objective: "Help Kodo get the milk", level: {
+  map:[
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0]
+  ],
+  player: {
+    x:0,
+    y:0
+  },
+  goal:{
+    x:4,
+    y:4
+  },
+  theme:'default'
+}
+)
+# -----------------------------------------
+#Level 4
+# Obj : Ajude Kodo a pegar os ovos
+Challenge.create(objective: "Help Kodo get the egg", level: {
+  map:[
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0]
+  ],
+  player: {
+    x:0,
+    y:4
+  },
+  goal:{
+    x:4,
+    y:0
+  },
+  theme:'default'
+}
+)
+# -----------------------------------------
+#Level 5
+# Obj : Ajude Kodo a pegar a farinha
+Challenge.create(objective: "Help Kodo get the flour", level: {
+  map:[
+    [0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0]
+  ],
+  player: {
+    x:0,
+    y:0
+  },
+  goal:{
+    x:4,
+    y:4
+  },
+  theme:'default'
+}
+)
+# -----------------------------------------
+#Level 6
+# Obj : Ajude Kodo a pegar o chocolate
+Challenge.create(objective: "Help Kodo get the chocolate", level: {
+  map:[
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0]
+  ],
+  player: {
+    x:0,
+    y:4
+  },
+  goal:{
+    x:4,
+    y:4
+  },
+  theme:'default'
+}
+)
+# -----------------------------------------
+#Level 7
+# Obj : Ajude Kodo a pegar o fermento
+Challenge.create(objective: "Help Kodo get the yeast", level: {
+  map:[
+    [0, 0, 0, 1, 0],
+    [1, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0]
+  ],
+  player: {
+    x:0,
+    y:0
+  },
+  goal:{
+    x:4,
+    y:0
+  },
+  theme:'default'
+}
+)
+# -----------------------------------------
+#Level 8
+# Obj : Ajude Kodo a voltar para casa
+Challenge.create(objective: "Help Kodo go back home", level: {
+  map:[
+    [0, 0, 0, 0, 0],
+    [1, 0, 1, 1, 1],
+    [0, 0, 0, 0, 0],
+    [1, 1, 1, 0, 1],
+    [0, 0, 0, 0, 0]
+  ],
+  player: {
+    x:4,
     y:0
   },
   goal:{
